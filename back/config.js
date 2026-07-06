513c31838f6f664815b2f5042e16912574339316
candymollier <candy.mollier@gmail.com>
2026-07-06 14:26:51 +0200

    organisation en dossiers

---
+++ back/config.js
@@ -0,0 +1,15 @@
+/**
+ * Réglages non secrets (ceux-ci restent visibles côté navigateur, mais ne posent aucun risque de sécurité).
+ */
+
+const CONFIG = {
+  // endroit
+  CENTER_LAT: 43.68649,   // nice villa mamours
+  CENTER_LON:7.28074,
+  RADIUS_KM: 4,
+
+  // autres infos
+  REFRESH_INTERVAL_MS: 15000,
+  MAX_FLIGHTS_DISPLAYED: 12,
+  ENABLE_AEROAPI: true
+};
